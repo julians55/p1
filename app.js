c3a3f9db2d8e58a9768d35591d742946784a38ad
Julian Salamanca <jsalamancah@unal.edu.co>
2022-07-17 01:16:30 -0500

    1

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+const http = require('http');
+
+const configExpress = require('./config/express');
+const app = express();
+var serialNumber = require('serial-number');
+const server = http.Server(app);
+
+app.use(express.urlencoded({extended: false}));
+configExpress(app);
+
+const port = process.env.PORT || 8081;
+
+app.get('/', (req, res) => {
+    
+    serialNumber(function (err, value) {
+        res.send(value);
+    });
+})
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+    serialNumber(function (err, value) {
+        console.log(value);
+    });
+  })
